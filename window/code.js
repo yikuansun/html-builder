1da72b4de159f15c1a6934d1b7449d05cd3dac83
Yikuan Sun <yikuansun2015@gmail.com>
2021-06-21 16:29:56 -0400

    clearTemp function

--- window/code.js
+++ window/code.js
@@ -2,11 +2,13 @@ const { remote, app } = require("electron");
 const { dialog } = remote;
 const admZip = require("adm-zip");
 const fs = require("fs");
-const child_process = require("child_process");
+const { exec } = require("child_process");
 
 var userDataPath = (app || remote.app).getPath("userData");
 
-if (fs.existsSync(userDataPath + "/temp")) fs.rmdirSync(userDataPath + "/temp", { recursive: true });
+function clearTemp() {
+    fs.rmdirSync(userDataPath + "/temp", { recursive: true });
+}
 
 var zip = new admZip();
 zip.addLocalFolder(__dirname + "/template");
