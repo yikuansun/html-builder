e8f7d53f3bac8a4a3e0acf55f5704e0a84838aea
Yikuan Sun <yikuansun2015@gmail.com>
2021-06-21 15:51:26 -0400

    Start to make the app thing

---
+++ window/code.js
@@ -0,0 +1,26 @@
+const { remote, app } = require("electron");
+const { dialog } = remote;
+const admZip = require("adm-zip");
+const fs = require("fs");
+const child_process = require("child_process");
+
+var userDataPath = (app || remote.app).getPath("userData");
+
+if (fs.existsSync(userDataPath + "/temp")) fs.rmdirSync(userDataPath + "/temp", { recursive: true });
+
+var zip = new admZip();
+zip.addLocalFolder(__dirname + "/template");
+zip.extractAllTo(userDataPath + "/temp", true);
+
+document.querySelector("#dirselect").onclick = function() {
+    var directory_to_add = dialog.showOpenDialogSync({
+        properties: ["openDirectory"]
+    });
+    if (directory_to_add) {
+        directory_to_add = directory_to_add[0];
+
+        var zip = new admZip();
+        zip.addLocalFolder(directory_to_add);
+        zip.extractAllTo(userDataPath + "/temp/app", true);
+    }
+};
