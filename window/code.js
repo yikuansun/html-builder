d6b02ba303103a27a08da607eb4dc3177a83fc62
Yikuan Sun <yikuansun2015@gmail.com>
2021-06-25 15:44:45 -0400

    Fix path for packaged app

--- window/code.js
+++ window/code.js
@@ -4,6 +4,9 @@ const admZip = require("adm-zip");
 const fs = require("fs");
 const { exec } = require("child_process");
 const png2icons = require("png2icons");
+const fixPath = require("fix-path");
+
+fixPath();
 
 var userDataPath = (app || remote.app).getPath("temp");
 
