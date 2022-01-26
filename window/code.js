7a01d1b87180d65b8a1f72daafafa596f00a5b77
yikuansun <yikuansun2015@gmail.com>
2022-01-25 20:56:26 -0500

    Get rid of png2icons
    No longer needed 'cause electron-builder is cool

--- window/code.js
+++ window/code.js
@@ -3,7 +3,6 @@ const { dialog } = remote;
 const admZip = require("adm-zip");
 const fs = require("fs");
 const { exec } = require("child_process");
-const png2icons = require("png2icons");
 const fixPath = require("fix-path");
 
 fixPath();
