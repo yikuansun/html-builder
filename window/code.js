43bf336175d8e6714e7b45c1c4f8e02c49f3063c
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-08-26 17:36:17 -0400

    Increase appId length

--- window/code.js
+++ window/code.js
@@ -51,7 +51,7 @@ document.querySelector("#iconselect").onclick = function() {
 document.querySelector("#appname").onchange = function() {
     var packagedata = JSON.parse(fs.readFileSync(userDataPath + "/temp/package.json"));
     packagedata["productName"] = this.value;
-    packagedata["build"]["appId"] = "com.electron.htmlapp" + Math.floor(Math.random() * 1000).toString();
+    packagedata["build"]["appId"] = "com.electron.htmlapp" + Math.floor(Math.random() * 10000).toString();
     fs.writeFileSync(userDataPath + "/temp/package.json", JSON.stringify(packagedata));
 };
 
