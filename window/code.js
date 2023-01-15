2761d39075b800aca9e38f6d66da71a91f049439
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-01-14 22:43:12 -0500

    Use html-builder-cli

--- window/code.js
+++ window/code.js
@@ -5,18 +5,16 @@ const admZip = require("adm-zip");
 const fs = require("fs");
 const { exec } = require("child_process");
 const fixPath = require("fix-path");
+const buildApp = require("html-builder-cli");
 
 fixPath();
 
 var userDataPath = app.getPath("temp");
 
-function clearTemp() {
-        fs.rmdirSync(userDataPath + "/temp", { recursive: true });
-}
-
-var zip = new admZip();
-zip.addLocalFolder(__dirname + "/template");
-zip.extractAllTo(userDataPath + "/temp", true);
+var userOptions = {
+    name: "", platforms: [], desc: "", version: "", icon: "", colorScheme: ""
+};
+var appPath = "";
 
 document.querySelector("#dirselect").onclick = function() {
     var directory_to_add = dialog.showOpenDialogSync({
@@ -26,10 +24,7 @@ document.querySelector("#dirselect").onclick = function() {
         directory_to_add = directory_to_add[0];
 
         document.querySelector("#dirlabel").innerText = directory_to_add;
-
-        var zip = new admZip();
-        zip.addLocalFolder(directory_to_add);
-        zip.extractAllTo(userDataPath + "/temp/app", true);
+        appPath = directory_to_add;
     }
 };
 
@@ -42,34 +37,25 @@ document.querySelector("#iconselect").onclick = function() {
     });
     if (file) {
         file = file[0];
-        if (!fs.existsSync(`${userDataPath}/temp/htmlbuilder-buildresources`)) fs.mkdirSync(`${userDataPath}/temp/htmlbuilder-buildresources`);
-        fs.copyFileSync(file, `${userDataPath}/temp/htmlbuilder-buildresources/icon.png`);
+        userOptions.icon = file;
         document.querySelector("#iconlabel").innerText = file;
     }
 };
 
 document.querySelector("#appname").onchange = function() {
-    var packagedata = JSON.parse(fs.readFileSync(userDataPath + "/temp/package.json"));
-    packagedata["productName"] = this.value;
-    packagedata["build"]["appId"] = "com.electron.htmlapp" + Math.floor(Math.random() * 10000).toString();
-    packagedata["name"] = "my-html-app" + Math.floor(Math.random() * 10000).toString();
-    fs.writeFileSync(userDataPath + "/temp/package.json", JSON.stringify(packagedata));
+    userOptions.name = this.value;
 };
 
 document.querySelector("#appdesc").onchange = function() {
-    var packagedata = JSON.parse(fs.readFileSync(userDataPath + "/temp/package.json"));
-    packagedata["description"] = this.value;
-    fs.writeFileSync(userDataPath + "/temp/package.json", JSON.stringify(packagedata));
+    userOptions.desc = this.value;
 };
 
 document.querySelector("#version").onchange = function() {
-    var packagedata = JSON.parse(fs.readFileSync(userDataPath + "/temp/package.json"));
-    packagedata["version"] = this.value;
-    fs.writeFileSync(userDataPath + "/temp/package.json", JSON.stringify(packagedata));
+    userOptions.version = this.value;
 };
 
 document.querySelector("#apptheme").onchange = function() {
-    fs.writeFileSync(userDataPath + "/temp/main.js", fs.readFileSync(__dirname + "/template/main.js", "utf-8").replace("{app_theme}", this.value));
+    userOptions.colorScheme = this.value;
 };
 
 document.querySelector("#package").onclick = function() {
@@ -83,19 +69,17 @@ document.querySelector("#package").onclick = function() {
 
     document.querySelector("#form").innerHTML = "Building in progress...";
 
-    exec(`cd "${userDataPath + "/temp"}"&& npm install&& npm run build-${platform}`, (error, stdout, stderr) => {
-        if (error) {
-            dialog.showErrorBox("Error!", error.message);
-            return;
-        }
-        /*if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }*/
-        console.log(`stdout: ${stdout}`);
+    buildApp(appPath, {
+        name: productName,
+        platforms: [ { "darwin": "mac", "linux": "linux", "win32": "windows" }[platform] ],
+        desc: userOptions.desc,
+        version: userOptions.version,
+        icon: userOptions.icon,
+        colorScheme: userOptions.colorScheme
+    });
 
         var zip = new admZip();
-        zip.addLocalFolder(`${userDataPath}/temp/dist`);
+        zip.addLocalFolder(`${appPath}/html-builder_output`);
 
         var downloadLocation = dialog.showSaveDialogSync(null, {
             defaultPath: `${(app || remote.app).getPath("downloads")}/${productName}`,
@@ -105,11 +89,9 @@ document.querySelector("#package").onclick = function() {
 
         zip.extractAllTo(downloadLocation);
 
-        clearTemp();
         document.querySelector("#form").innerHTML = `
             <h2>Thank you for using HTML Builder!</h2>
             <button onclick="shell.openExternal('https://github.com/yikuansun/html-builder/issues')" class="button">Submit feedback</button>
             <br /> <button onclick="location.reload();" class="button">Make another app</button>
         `;
-    });
 };
